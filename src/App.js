e334e6f3c48339661830c8ea485901522ce34c84
nmx88 <ioannislagos88@gmail.com>
2020-05-29 18:55:34 +0300

    Presenting you the Ravenous Project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
+import Yelp from './util/Yelp';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       businesses : [],
+     };
+     
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+   Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.setState({businesses: businesses });
+   });
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>nmx88 some rip of yelp </h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
+export default App;
